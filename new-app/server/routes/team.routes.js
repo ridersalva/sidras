542ad725631f1d655a3439a11ff44a540640412d
salva <saersalva@gmail.com>
2022-05-17 23:08:57 +0200

    .env listo

--- new-app/server/routes/team.routes.js
+++ new-app/server/routes/team.routes.js
@@ -17,28 +17,28 @@ router.get('/teamView', isAuthenticated, (req, res, next) => {
 
 router.post('/create', isAuthenticated, (req, res, next) => {
     TeamMember
-    .create(req.body)
-    .then(result=>res.status(200).json(result))
-    .catch(err=> res.status(500).json(err))
+        .create(req.body)
+        .then(result => res.status(200).json(result))
+        .catch(err => res.status(500).json(err))
 
 })
 
 //Seleccionar uno 
 
-router.get(`/:teamMember_id`,(req,res)=>{
-    const{ teamMember_id}= req.params
+router.get(`/:teamMember_id`, (req, res) => {
+    const { teamMember_id } = req.params
     TeamMember
-    .findById(teamMember_id)
-    .then(result=> res.json(result))
-    .catch(err=>res.status(500).json(err))
+        .findById(teamMember_id)
+        .then(result => res.json(result))
+        .catch(err => res.status(500).json(err))
 })
 
 //Editar uno
 
-router.put('/:teamMember_id',isAuthenticated,(req,res)=>{
-    const{ teamMember_id}=req.params
+router.put('/:teamMember_id', isAuthenticated, (req, res) => {
+    const { teamMember_id } = req.params
     TeamMember
-    .findByIdAndUpdate(teamMember_id,{...req.body},{new:true})
-    .then(result=> res.status(200).json(result))
-    .catch(err=>res.status(500).json(err))
+        .findByIdAndUpdate(teamMember_id, { ...req.body }, { new: true })
+        .then(result => res.status(200).json(result))
+        .catch(err => res.status(500).json(err))
 })
